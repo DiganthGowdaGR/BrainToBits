6697e05026917bba2192d32d9f2c183f1efe1134
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-06-17 11:27:19 +0530

    Create vari.java
    this variable types in JAVA

---
+++ vari.java
@@ -0,0 +1,13 @@
+        // Variables and Data Types
+        // int, double, String, char, Boolean
+
+class vari{
+    public static void main(String[] args) {
+       int age = 20;
+       double pi = 3.14;
+       char letter = 'A';
+       String name = "Coder";
+       boolean isJavaGood = true;
+
+    }
+}
