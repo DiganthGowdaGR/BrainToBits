d406465278b1c548649671c72e58ccad1054e7db
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-06-17 17:38:11 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+// Conditionale
+// if, else, if-else
+// Using conditionale find a large number
+
+class main{
+   public  static void main  (String arg[]){
+    int a,b;
+    a = 10;
+    b = 20;
+
+    if(a>b){
+        System.out.println("Largest ::"+a);
+    }
+    else{
+        System.out.println("Largest ::"+b);
+    }
+   }  
+}
